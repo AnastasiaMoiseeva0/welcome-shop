0503c96034636c1ec25e7861524d7ffe775bd53f
AnastasiaMoiseeva0 <Shinaeva1997@mail.ru>
2023-06-01 20:38:33 +0300

    fead: добавлен блок lead

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+function Main() {
+  return (
+    <div>
+      <main className="main-content">
+        <section className="lead">
+          <div className="lead__image">
+            <hi className="lead__title">welcome</hi>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default Main;
